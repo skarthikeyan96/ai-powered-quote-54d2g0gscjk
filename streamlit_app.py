5d99393594997e203234c817ee42afbac14052b2
Karthikeyan <imkarthikeyans@gmail.com>
2024-07-06 19:51:10 +0530

    add open ai prompt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,33 @@
 import streamlit as st
 
+
 st.title("🎈 AI powered quote generator")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
+
+def generate_response(text):
+    # Instantiate LLM model
+    llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
+    # Prompt
+    template = "Generate a {text} quote. The quote should be concise, impactful and suitable for sharing on social media."
+    prompt = PromptTemplate(input_variables=["text"], template=template)
+    prompt_query = prompt.format(topic=text)
+    # Run LLM model
+    response = llm(prompt_query)
+    # Print results
+    return st.info(response)
+    print(text)
 
 with st.form('form'):
     option = st.selectbox(
     "Select a theme",
     ("inspirational ", "motivational", "humor", "wisdom"))
     submitted = st.form_submit_button('submit')
+
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+elif submitted:
+    generate_response(text)
