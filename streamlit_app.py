e7b6dc3ba147891aa275c74aacf2b3b2354d58a6
Karthikeyan <imkarthikeyans@gmail.com>
2024-07-06 18:58:14 +0530

    add way to enter the key

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
