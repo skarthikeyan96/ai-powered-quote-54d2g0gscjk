f08222bd4557b2e345dee91b91aca7dd04530648
Karthikeyan <imkarthikeyans@gmail.com>
2024-07-06 19:05:18 +0530

    add ui elements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 AI powered quote generator")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
@@ -8,3 +8,9 @@ st.write(
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+
+with st.form('form'):
+    option = st.selectbox(
+    "Select a theme",
+    ("inspirational ", "motivational", "humor", "wisdom"))
+    submitted = st.form_submit_button('submit')
