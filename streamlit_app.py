07afe9353a6fb951710f25d1f72b9e1c8dff18f2
Karthikeyan <imkarthikeyans@gmail.com>
2024-07-08 18:54:31 +0530

    final commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,8 @@
-# import streamlit as st
-# from langchain.llms import OpenAI
-# from langchain.prompts import PromptTemplate
-
-
-# st.title("🎈 AI powered quote generator")
-
-# with st.sidebar:
-#     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-#     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-
-
-# def generate_response(text):
-#     print(text)
-#     # # Instantiate LLM model
-#     # llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
-#     # # Prompt
-#     # template = "Generate a {text} quote. The quote should be concise, impactful and suitable for sharing on social media."
-#     # prompt = PromptTemplate(input_variables=["text"], template=template)
-#     # prompt_query = prompt.format(topic=text)
-#     # # Run LLM model
-#     # response = llm(prompt_query)
-#     # # Print results
-#     # return st.info(response)
-#     # print(text)
-
-# with st.form('form'):
-#     option = st.selectbox(
-#     "Select a theme",
-#     ("inspirational ", "motivational", "humor", "wisdom"))
-#     submitted = st.form_submit_button('submit')
-
-# # if not openai_api_key:
-# #     st.info("Please add your OpenAI API key to continue.")
-# # elif submitted:
-#     generate_response(option)
-
 # Import necessary libraries
 import os
 import streamlit as st
 import google.generativeai as genai 
 from dotenv import load_dotenv
-from PIL import Image
 
 # Load environment variables from .env file
 load_dotenv()
@@ -53,8 +15,7 @@ genai.configure(api_key=api_key)
 
 # Set the page configuration for the Streamlit app
 st.set_page_config(
-    page_title="Google Gemini Models",
-    page_icon="🤖"
+    page_title="AI powered quote generator"
 )
 
 def generate_response(text):
