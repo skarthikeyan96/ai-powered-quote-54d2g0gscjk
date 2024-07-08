dd0ec3dc9506909bcd43432595e31eafca569612
Karthikeyan <imkarthikeyans@gmail.com>
2024-07-08 18:26:51 +0530

    change model

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ def generate_response(text):
         
     prompt = f"""Generate a {text} quote. The quote should be concise, impactful and suitable for sharing on social media.
     """
-    model = genai.GenerativeModel('gemini-1.5-flash')
+    model = genai.GenerativeModel('gemini-1.0-pro-latest')
     response = model.generate_content(prompt)
     
     st.write(response.text)
